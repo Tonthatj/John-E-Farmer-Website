9a401b22a1c1ef5151270f40322d7ea3cd606f54
Tonthatj <36476537+Tonthatj@users.noreply.github.com>
2018-10-23 09:18:35 -0400

    home.php
    New Website

---
+++ home.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>John E. Farmer & Son's</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/full-slider.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.js"></script>
+    <style>
+        .carousel-item {
+            height: 100vh;
+            min-height: 300px;
+            background: no-repeat center center scroll;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+        #about { padding-top: 65px; }
+        #portfolio { padding-top: 65px; }
+        #services { padding-top: 65px; }
+        #contact { padding-top: 65px; }
+        
+        
+    </style>
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top text-danger">
+      <div class="container">
+        <a class="navbar-brand text-danger" href="#"><h2>John E. Farmer & Son's Construction</h2></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            
+            <li class="nav-item">
+              <a class="nav-link text-danger" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-danger" href="#portfolio">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-danger" href="#services">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-danger bg-warning" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    
+
+    <header>
+      <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel" data-interval="4000" data-wrap="true" data-pause="false">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+          <!-- Slide One - Set the background image for this slide in the line below -->
+          <div class="carousel-item active" style="background-image: url(8.jpg)">
+            <div class="carousel-caption d-none d-md-block text-danger bg-dark rounded-circle">
+              <h3>Providing the services you need since 1974!</h3>
+              <p></p>
+            </div>
+          </div>
+          <!-- Slide Two - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url(mining.jpg)">
+            <div class="carousel-caption d-none d-md-block text-danger bg-dark rounded-circle">
+              <h3>Over 60 years of experience in our field!</h3>
+              <p></p>
+            </div>
+          </div>
+          <!-- Slide Three - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url(crawler.jpg)">
+            <div class="carousel-caption d-none d-md-block text-danger bg-dark rounded-circle">
+              <h3>Fully Bonded and Insured!</h3>
+              <p></p>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </header>
+
+    
+
+    <!-- Page Content -->
+    <section class="py-5">
+    <div class="container">
+  <div class="row">
+    
+    <div id="about" class="col-sm-9">
+      <h2>What We Do</h2>
+      <p1> Drainage Contractors that have over 30 years of experience which are utilized to provide exemplary work.
+      Our garage is located on the Burlington Billerica line. Lorem ipsum dolor sit amet, consectetur adipiscing 
+      elit. Fusce augue orci, varius ac sollicitudin a,finibus quis magna. Nulla orci erat, dapibus quis dui sed,
+      sodales cursus augue. Vestibulum id ante vestibulum sem accumsan bibendum. Ut imperdiet lorem a lorem congue.
+      tellus aliquam, eleifend purus et, iaculis ipsum. Etiam nec eros at velit facilisis mollis. Pellentesque
+      lacinia pharetra sem tincidunt aliquet. Aliquam erat volutpat. Suspendisse magna eros,
+      fringilla sit amet dapibus non, iaculis vel quam. Mauris eu consequat tellus. Suspendisse
+      ut metus justo.  
+      </p1>
+    </div>
+
+    <div id="contact" class="col-sm-3">
+      <h2>Contact Us</h2>
+      <p>Contact us and we'll get back to you within 48 hours.</p>
+      <p><span class="glyphicon glyphicon-map-marker"></span>Lexington Rd, Billerica, MA 01821A</p>
+      <p><span class="glyphicon glyphicon-phone"></span>Office: 978-667-8184</p>
+      <p><span class="glyphicon glyphicon-phone"></span>Cell: 978-804-3792</p>
+    </div>    
+  </div>
+    
+    
+
+  <p></p>
+  <div id="portfolio">
+  <h2>Past Projects</h2>
+  <div class="row">
+      <div class="col-sm-3">
+        <img src="past1.jpeg" alt="Flowers in Chania" style="width:200px;height:200px;">
+      </div>
+     <div class="col-sm-8">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce augue orci, varius ac sollicitudin a,
+      finibus quis magna. Nulla orci erat, dapibus quis dui sed, sodales cursus augue. Vestibulum id ante
+      vestibulum sem accumsan bibendum. Ut imperdiet lorem a lorem congue, ut varius mauris aliquet. Ut eget
+      tellus aliquam, eleifend purus et, iaculis ipsum. Etiam nec eros at velit facilisis mollis. Pellentesque
+      lacinia pharetra sem tincidunt aliquet. Aliquam erat volutpat. Suspendisse magna eros,
+      fringilla sit amet dapibus non, iaculis vel quam. Mauris eu consequat tellus. Suspendisse
+      ut metus justo.  
+      </p>
+    </div>
+  </div>
+  <p> </p>
+
+  <div class="row">
+      <div class="col-sm-3">
+        <img src="past2.jpeg" alt="Flowers in Chania" style="width:200px;height:200px;">
+      </div>
+     <div class="col-sm-8">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce augue orci, varius ac sollicitudin a,
+      finibus quis magna. Nulla orci erat, dapibus quis dui sed, sodales cursus augue. Vestibulum id ante
+      vestibulum sem accumsan bibendum. Ut imperdiet lorem a lorem congue, ut varius mauris aliquet. Ut eget
+      tellus aliquam, eleifend purus et, iaculis ipsum. Etiam nec eros at velit facilisis mollis. Pellentesque
+      lacinia pharetra sem tincidunt aliquet. Aliquam erat volutpat. Suspendisse magna eros,
+      fringilla sit amet dapibus non, iaculis vel quam. Mauris eu consequat tellus. Suspendisse
+      ut metus justo.  
+      </p>
+    </div>
+  </div>
+
+<p> </p>
+<p> </p>
+
+
+  <div class="container" id="services">
+  <h2>Services</h2>
+  <div class="row">
+    <div  class="col-sm-8">
+      <h4>Site Development</h4>
+      <p1>John E. Farmer & Son’s Site Development services include site and utility demolition,
+       land clearing and grubbing, erosion controls, site cuts and fills, building foundation excavation and 
+       backfill, drilling and blasting, water, sewer and drainage, paving and curbing.
+      </p1>
+    </div>
+    <div class="col-sm-3">
+        <img src="past2.jpeg" alt="Flowers in Chania" style="width:200px;height:200px;">
+    </div>
+    </div>
+    <p> </p>
+
+
+
+    <div class="row">
+    <div id="about" class="col-sm-8">
+
+      <h4>Utility Construction</h4>
+      <p1> John E. Farmer & Son's  Utility Construction & Installation services include sanitary sewer work
+       such as piping, holding tanks, grease traps and pumping stations. WLF’s drainage services includes piping,
+        detention tanks, infiltration systems and water quality units.  WLF also performs the installation
+        of water distribution piping.
+      </p1>
+
+    </div>
+    <div class="col-sm-3">
+        <img src="past1.jpeg" alt="Flowers in Chania" style="width:200px;height:200px;">
+    </div>
+
+    </div>
+    
+
+    
+  </div>
+
+</div>
+  
+  </div>
+</div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark text-danger">
+      <div class="container">
+        <p class="m-0 text-center">Copyright &copy; JohnEFarmer.com 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
