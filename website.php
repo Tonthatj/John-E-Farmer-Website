43fa49dd214f505d1e36571ebbd01c2c1003f81f
Tonthatj <36476537+Tonthatj@users.noreply.github.com>
2018-11-12 15:42:38 -0500

    Revised website

---
+++ website.php
@@ -0,0 +1,386 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>John E. Farmer & Son's</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/full-slider.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.css">
+    <link href="https://fonts.googleapis.com/css?family=Cinzel|Lobster|Monoton|Sacramento|Ultra|Actor" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.js"></script>
+    <script language="javascript">
+
+      function toggle1() 
+      {
+      
+      }
+    </script>
+
+    <style>
+        html {
+          overflow: scroll;
+          overflow-x: hidden;
+          width:100%;
+        }
+        a:hover { 
+          color: red;
+        }
+        p{
+          text-align:justify;
+          font-family: 'Actor', serif;
+        }
+        .navbar {
+          padding-top:10px;
+          padding-bottom:10px;
+          background-color: transparent;
+          border: 0px solid black;
+        }
+        .nav-link {
+          font-family: 'Ultra', cursive;
+          color: black;
+        }
+        .navbar-brand {
+          font-family: 'Sacremento', cursive;
+          color: black;
+        }
+        .relative {
+          overflow: scroll; 
+          position:absolute;
+          height:90vh;
+          width:100%;
+          border-width: medium;
+          
+        }
+        .carousel-item {
+          height:200px;
+          width:150px;
+          background: no-repeat center center;
+          background-size: cover;
+        }
+        .carousel-indicators li { visibility: hidden; }
+        strong{
+          font-family: 'Cinzel', serif;
+          color: red;
+        }
+        #about {
+          padding-left:100px;
+          padding-right:100px;
+          padding-top:25px;
+          padding-bottom:25px;
+          border-top: 0px solid black;
+
+        }
+        #services {
+          padding-left:100px;
+          padding-right:100px;
+          padding-top:20px;
+          padding-bottom:20px;
+          border-top: 0px solid black;
+        }
+
+        #portfolio {
+          padding-left:100px;
+          padding-right:100px;
+          padding-top:20px;
+          padding-bottom:20px;
+          border-top: 0px solid black;
+        }
+
+        #contact {
+          padding-left:100px;
+          padding-right:100px;
+          padding-top:10px;
+          padding-bottom:10px;
+          border-top: 0px solid black;
+        }
+
+        #footer {
+          padding-left:100px;
+          padding-right:100px;
+          padding-top:0px;
+          padding-bottom:0px;
+          border-top: 2px solid black;
+        }
+
+        #banner {
+          width:100%;
+          padding-left:30px;
+          border-top: 0px solid black;
+        }
+        
+        h4
+        {
+          font-family: 'Ultra', cursive;
+        }
+        
+
+    </style>
+
+  </head>
+
+  <body style="background-color:	#DCDCDC">
+
+    <!-- Navigation -->
+    
+
+    <nav class="navbar navbar-expand-lg ">
+      <div class="container">
+
+        <div class="collapse navbar-collapse " id="navbarResponsive">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link " href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#services">Services</a>
+            </li>
+          </ul>
+        </div>
+
+        <a class="navbar-brand" href="#"> <h2>John E. Farmer & Son's Construction</h2> </a>
+
+        
+
+        <div class="collapse navbar-collapse " id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link " href="#portfolio">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#contact">Contact</a>
+            </li>           
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
+    <div id="banner" >
+      <img src="assets/cmbanner.jpg" alt="Banner" style="width:100%; height:350px;">
+    </div>
+
+    <!-- About -->
+
+    <div class="row" id="about" >
+
+            <div class="col-sm-8" >
+              <p> <h3> <i> Providing the services you need since 1974! </i> </h3> </p>
+              <p style="text-align:justify;">
+              John Farmer & Sons is a fully licensed and insured contractor with over 60 years experience in the 
+              construction industry. John E Farmer & Sons is a small firm that specializes in one on one interactions with 
+              clients to accomplish their goals through out all phases of a project. Our projects range from indiviual house 
+              sites to large commercial projects and subdivisions. John Farmer & Sons is located in Billerica MA, and services 
+              the local area as well as southern New Hampshire. The company is run by John Farmer Sr., John Farmer Jr., and Mark Farmer. 
+              </p>
+            </div>
+
+            <div class="col-sm-4" style="padding-left: 200px;">
+
+              <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel" data-interval="4000" data-wrap="true" data-pause="false">
+                
+                <ol class="carousel-indicators">
+                  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+
+                <div class="carousel-inner" role="listbox">
+                <!-- Slide One - Set the background image for this slide in the line below -->
+                  <div class="carousel-item active" style="background-image: url(assets/john.jpg)">
+                    <div class="carousel-caption d-none d-md-block"> </div>
+                  </div>
+
+                <!-- Slide Two - Set the background image for this slide in the line below -->
+                  <div class="carousel-item" style="background-image: url(assets/johnny.jpg)">
+                    <div class="carousel-caption d-none d-md-block"> </div>
+                  </div>
+
+                <!-- Slide Three - Set the background image for this slide in the line below -->
+                  <div class="carousel-item" style="background-image: url(assets/mark.jpg)">
+                    <div class="carousel-caption d-none d-md-block"> </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+
+
+    </div>
+
+    <!-- Services -->
+
+    <div id="services">
+            <p> <center> <h4> Services </h4> </center> </p>
+            <div class="row">
+
+              <!-- site dev -->
+              <div class="col-sm-3">
+                <img src="assets/sticker1.jpg" alt="SiteDev" style="width:150px;height:150px; margin-left: auto; margin-right: auto; display: block;">
+                <p style ="text-align:justify;"> 
+                <center> <strong> Site Development </strong> </center>
+                </p>
+                <p style ="text-align:justify;">
+                John E. Farmer & Son’s Site Development services include site and utility demolition,
+                land clearing and grubbing, erosion controls, site cuts and fills, building foundation excavation and 
+                backfill, water, sewer and drainage, and retaining wall installation.
+                </p>
+              </div>
+              <!-- utility -->
+              <div class="col-sm-3">
+                <img src="assets/sticker2.jpg" alt="UtilCon" style="width:150px;height:150px; margin-left: auto; margin-right: auto; display: block;">
+                <p style ="text-align:justify;"> 
+                <center> <strong> Utility Construction </strong> </center>
+                </p>
+                <p style ="text-align:justify;">
+                John E. Farmer & Son's  Utility Construction & Installation services include sanitary sewer work
+                such as piping, holding tanks, grease traps and pumping stations. JEF’s drainage services includes piping,
+                detention tanks, infiltration systems and water quality units.  JEF also performs the installation
+                of water distribution piping.                     
+                </p>
+              </div>
+              
+              <!-- loam -->
+              <div class="col-sm-3">
+                <img src="assets/sticker3.jpg" alt="Loam" style="width:150px;height:150px; margin-left: auto; margin-right: auto; display: block;">
+                <p style ="text-align:justify;"> 
+                <center> <strong> Loam, Gravel, and Stones </strong> </center>
+                </p>
+                <p style ="text-align:justify;">
+                John E. Farmer & Son's sells loam, gravel, and stone. Pricing may vary depending on distance required to haul the material.                
+                </p>
+              </div>
+
+              <!-- other -->
+              <div class="col-sm-3">
+                <img src="assets/sticker4.jpg" alt="Other" style="width:150px;height:150px; margin-left: auto; margin-right: auto; display: block;">
+                <p style ="text-align:justify;"> 
+                <center> <strong> Other </strong> </center>
+                </p>
+                <p style ="text-align:justify;">
+                John E. Farmer & Son's  has performed a variety of services that include golf course construction and sport fields. 
+                </p>
+              </div>
+              
+            </div>
+    </div>
+
+    <!-- Past Projects -->
+
+    <div id="portfolio">
+            <p> <center> <h4> Past Projects </h4> </center> </p>
+            <div class="row">
+
+              <!-- Tallyho -->
+              <div class="col-sm-4">
+                <img src="assets/smollack.jpg" alt="Meeting House" style="width:px;height:150px; margin-left: auto; margin-right: auto; display: block;">
+                <p style ="text-align:justify;"> 
+                <center> <strong> Meeting House Commons </strong> </center>
+                </p>
+                <p style ="text-align:justify;">
+                A 30-acre 88-unit complex in North Andover. The project 
+                was developed under the Massachusetts' Affordable Housing Zoning Law. John E. Farmer & Sons Inc. installed erosion controls, carved out
+                roads, erected retaining walls, and installed the underground utilites including water, sewer, and drainage.
+                </p>
+              </div>
+              <!-- Huit -->
+              <div class="col-sm-4" >
+                <img src="assets/burlingtonheights.jpg" alt="Burlington Heights" style="width:200px;height:150px; margin-left: auto; margin-right: auto; display: block;">
+                <p style ="text-align:justify;"> 
+                <center> <strong> Burlington Heights </strong> </center>
+                </p>
+                <p style ="text-align:justify;">
+                A 37-unit garden style condo project, is located at 235-245 Cambridge Street, Burlington MA. 
+                John Farmer & Sons Inc. installed an extensive retaining wall system prior to carving out the hillside due to the 
+                challenging site conditions and the proximinty of the proposed buildings to Cambridge St. John E. Farmer & Sons also 
+                installed the water, sewer, drainage systems, excavated and backfilled the foundation and prepared the site for the bitumunious
+                concrete.     
+                </p>
+              </div>
+              <!-- Erba -->
+              <div class="col-sm-4" >
+                <img src="assets/braemor.jpg" alt="Braemoor Woods" style="width:200px;height:150px; margin-left: auto; margin-right: auto; display: block;">
+                <p style ="text-align:justify;"> 
+                <center> <strong> Braemoor Woods </strong> </center>
+                </p>
+                <p style ="text-align:justify;">
+                A 55 and over adult community located in Salem NH. John Farmer & Sons erected retaining walls,
+                and installed the underground utilites including water, sewer, and drainage. John Farmer & Sons also excavated and 
+                backfilled the foundation for both apartments.                
+                </p>
+              </div>
+              
+            </div>
+    </div>
+
+    <!-- Contact Us-->
+
+    <div id="contact">
+      <p>  <center>  <h4> Contact Us </h4> </center> </p>
+      <div class="row" style="padding-left:100px;"> 
+        <div class="col-sm-6">
+          <p>Office: 19 Ansley Circle, Billerica, MA 01821A <br> Garage: Lexington Rd, Billerica, MA 01821A</p>
+        </div>
+        <div class="col-sm-6">
+          <div class="row">
+            <div class="col-sm-2">
+              <p class="tab">Office: <br> Cell: <br> Fax:</p>
+            </div>
+            <div class="col-sm-3">
+              <p class="tabl"> 978-667-8184<br> 978-804-3792 <br> 978-670-9881</p>
+            </div>
+          </div>
+        </div> 
+
+      </div>
+    </div>
+
+
+    <div class="row" id="footer">
+
+      <div class="col-sm-3">
+        <a href="http://www.facebook.com">
+          <button type="button" href=”www.facebook.com” style="margin-left: auto; margin-right: auto; display: block;"> 
+            <img src="assets/Facebook.jpg" alt="Braemoor Woods" style="width:100px;height:70px; ">
+          </button>
+        </a>
+      </div>
+
+      <div class="col-sm-6">
+        <p>  <center> Handcrafted by Omni-ChannelGroup &copy; 2018  </center>   </p>
+      </div>
+
+      <div class="col-sm-3">
+        <a href="http://www.google.com">
+          <button type="button" style="margin-left: auto; margin-right: auto; display: block;"> 
+            <img src="assets/Google.jpg" alt="Braemoor Woods" style="width:100px;height:70px;">
+          </button>
+        </a>
+      </div>
+
+    </div>
+
+      
+
+
+  
+    
+
+    
+
+    
+    
+
+  </body>
+
+</html>
